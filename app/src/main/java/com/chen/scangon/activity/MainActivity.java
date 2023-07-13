8c51615b7a403cf5a4695019d5ef1d19cced05c0
Your Name <you@example.com>
2023-07-13 08:13:02 +0000

    usb key test ok

---
+++ app/src/main/java/com/chen/scangon/activity/MainActivity.java
@@ -0,0 +1,120 @@
+package com.chen.scangon.activity;
+
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chen.scangon.adapter.ScanAdapter;
+import com.chen.scangon.helper.ScanGunKeyEventHelper;
+import com.example.scan_module_add_key_demo.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements ScanGunKeyEventHelper.OnScanSuccessListener {
+
+
+    private ListView lv_main;
+    private TextView tv_main_title_num;
+    private ScanAdapter mAdapter;
+    private ArrayList<String> mList;
+    private ScanGunKeyEventHelper mScanGunKeyEventHelper;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        initView();
+        intData();
+    }
+
+
+
+    private void initView() {
+        lv_main = (ListView) findViewById(R.id.lv_main);
+        tv_main_title_num = (TextView) findViewById(R.id.tv_main_title_num);
+    }
+
+    private void intData() {
+        mContext = this;
+        mList = new ArrayList<>();
+        mAdapter = new ScanAdapter(mContext, mList);
+        lv_main.setAdapter(mAdapter);
+        registerBoradcastReceiver();
+        mScanGunKeyEventHelper = new ScanGunKeyEventHelper(this);
+    }
+
+    /**
+     * 截获按键事件.发给ScanGunKeyEventHelper
+     *
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
+        //if (mScanGunKeyEventHelper.isScanGunEvent(event)) {
+            mScanGunKeyEventHelper.analysisKeyEvent(event);
+           return true;
+       // }
+      //  return super.dispatchKeyEvent(event);
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!mScanGunKeyEventHelper.hasScanGun()) {
+            Toast.makeText(mContext, "未检测到扫码枪设备", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(stateChangeReceiver);
+        mScanGunKeyEventHelper.onDestroy();
+    }
+
+    @Override
+    public void onScanSuccess(String barcode) {
+        mList.add(barcode);
+        tv_main_title_num.setText("" + mList.size());
+        mAdapter.notifyDataSetChanged();
+    }
+
+
+
+    private void registerBoradcastReceiver() {
+        IntentFilter filter1 = new IntentFilter(
+                BluetoothAdapter.ACTION_STATE_CHANGED);
+        IntentFilter filter2 = new IntentFilter(
+                BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED);
+        registerReceiver(stateChangeReceiver, filter1);
+        registerReceiver(stateChangeReceiver, filter2);
+    }
+
+    private BroadcastReceiver stateChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                Toast.makeText(MainActivity.this, "蓝牙设备连接状态已变更", Toast.LENGTH_SHORT).show();
+            } else if (action.equals(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED)) {
+                Toast.makeText(MainActivity.this, "蓝牙设备连接状态已变更", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+}
--- app/src/main/java/com/example/scan_module_add_key_demo/MainActivity.java
+++ app/src/main/java/com/example/scan_module_add_key_demo/MainActivity.java
@@ -6,26 +6,33 @@ import android.app.Activity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class  MainActivity extends Activity {
 
 	EditText editText;
-
+    TextView TextViewCode;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		editText=(EditText)findViewById(R.id.editCode);
+        TextViewCode =findViewById(R.id.TextViewCode);
 		final Button open = (Button)findViewById(R.id.btn_scan);
         open.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				editText.setText("");
-
+                TextViewCode.setText("");
 			}
 		});		
 	}
@@ -58,7 +65,87 @@ public class  MainActivity extends Activity {
         char c1 = hexDigits[bt & 0xf];// ȡ�ֽ��е� 4 λ������ת��   
         stringbuffer.append(c0);  
         stringbuffer.append(c1);  
-    }  	
-	
+    }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        return false;
+    }
+/*
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        checkLetterStatus(event);
+        keyCodeToNum(keyCode);
+        if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+            Log.e("ca1","键盘事件"+ buffer.toString());
+            buffer.delete(0, buffer.length());
+            return true;
+        }
+
+        return false;
+    }
+
+
+    //检查shift键
+    private void checkLetterStatus(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        if (keyCode == KeyEvent.KEYCODE_SHIFT_RIGHT || keyCode == KeyEvent.KEYCODE_SHIFT_LEFT) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                //按着shift键，表示大写
+                mCaps = true;
+            } else {
+                //松开shift键，表示小写
+                mCaps = false;
+            }
+        }
+    }
+    //根据keycode得到对应的字母和数字
+    private void keyCodeToNum(int keycode) {
+        if (keycode >= KeyEvent.KEYCODE_A && keycode <= KeyEvent.KEYCODE_Z) {
+            if (mCaps) {
+                buffer.append(map.get(keycode).toUpperCase());
+            } else {
+                buffer.append(map.get(keycode));
+            }
+
+        } else if ((keycode >= KeyEvent.KEYCODE_0 && keycode <= KeyEvent.KEYCODE_9)) {
+            buffer.append(keycode - KeyEvent.KEYCODE_0);
+        } else {
+            //暂不处理特殊符号
+        }
+
+    }
+
+
+    static Map<Integer, String> map = new HashMap<>();
+   static {
+
+       map.put(29, "a");
+       map.put(30, "b");
+       map.put(31, "c");
+       map.put(32, "d");
+       map.put(33, "e");
+       map.put(34, "f");
+       map.put(35, "g");
+       map.put(36, "h");
+       map.put(37, "i");
+       map.put(38, "g");
+       map.put(39, "k");
+       map.put(40, "l");
+       map.put(41, "m");
+       map.put(42, "n");
+       map.put(43, "0");
+       map.put(44, "p");
+       map.put(45, "q");
+       map.put(46, "r");
+       map.put(47, "s");
+       map.put(48, "t");
+       map.put(49, "u");
+       map.put(50, "v");
+       map.put(51, "w");
+       map.put(52, "x");
+       map.put(53, "y");
+       map.put(54, "z");
+    }*/
 }
